8f6f19e81ffa5f5aea19912dcf6b804bcb628bd8
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-05-26 15:12:40 +0530

    Patch : added up necessary code and requirements
    I've debugged the code and the current code is working completely fine. :)

--- voice.py
+++ voice.py
@@ -1,22 +1,21 @@
-import speech_recognition
+import speech_recognition as sr
 from gtts import gTTS 
 import os
 
 voice=""
 
 while True:
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
-        audio = r.listen(source)
-        text = r.recognize_google(audio)
         try:
+            audio = r.listen(source)
+            text = r.recognize_google(audio, language = 'en', show_all = True)
             print(text)
-            if text=="stop":
+            if text == "stop" or text == "exit":
                 break
-            voice=voice+str(text)
+            voice = voice + str(text)
+        except:
+            print("say something...") 
 
-            except:
-                print("say something...") 
-
-hr=gTTS(text=voice,lang='en', slow=False)
+hr=gTTS(text = voice, lang='en', slow = False)
 hr.save("1.wav")
